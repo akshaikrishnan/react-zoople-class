f87bdfc652ad89f0cbfeda5c95345f2563208698
akshaikrishnan <71927833+akshaikrishnan@users.noreply.github.com>
2024-01-23 19:28:07 +0530

    todo list with complete status

--- src/components/to-do/Todo.tsx
+++ src/components/to-do/Todo.tsx
@@ -2,14 +2,19 @@ import { useState } from "react";
 import TodoList from "./TodoList";
 
 export default function Todo() {
-  const [taskName, setTaskName] = useState("");
+  const [taskName, setTaskName] = useState({ name: "", isCompleted: false });
   const [tasks, setTasks] = useState<any>([]);
 
   const addTask = () => {
     setTasks([...tasks, taskName]);
-    setTaskName("");
+    setTaskName({ name: "", isCompleted: false });
   };
 
+  const removeTask = (index: number) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
   return (
     <div className="container">
       <div className="mb-3">
@@ -21,8 +26,10 @@ export default function Todo() {
           className="form-control"
           id="exampleFormControlInput1"
           placeholder="task name"
-          value={taskName}
-          onChange={(e) => setTaskName(e.target.value)}
+          value={taskName.name}
+          onChange={(e) => {
+            setTaskName({ name: e.target.value, isCompleted: false });
+          }}
         />
       </div>
       <div className="d-grid">
@@ -30,7 +37,7 @@ export default function Todo() {
           Add Task
         </button>
       </div>
-      <TodoList tasks={tasks} />
+      <TodoList remove={removeTask} tasks={tasks} />
     </div>
   );
 }
--- src/components/to-do/TodoList.tsx
+++ src/components/to-do/TodoList.tsx
@@ -1,10 +1,20 @@
-export default function TodoList({ tasks }: any) {
+export default function TodoList({ tasks, remove }: any) {
   return (
     <div className="pt-3">
       <h5 className="text-center mb-3"> My Tasks</h5>
-      {tasks.map((item: string, index: number) => (
-        <div key={index} className="card p-3 shadow-sm mb-2">
-          {item}
+      {tasks.map((item: any, index: number) => (
+        <div
+          key={index}
+          className="card p-3 shadow-sm mb-2 d-flex flex-row justify-content-between align-items-center"
+        >
+          <span className="">{item.name}</span>- {item.isCompleted.toString()}
+          <button
+            onClick={() => remove(index)}
+            className="btn btn-sm btn-danger"
+          >
+            {" "}
+            Delete
+          </button>
         </div>
       ))}
     </div>
