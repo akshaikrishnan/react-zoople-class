4db91cf691d128425ca5d31dfee80f18654e93fd
Saurav S <sauravsvtk@gmail.com>
2024-01-24 17:44:40 +0530

    added checkbox function

--- src/components/to-do/Todo.tsx
+++ src/components/to-do/Todo.tsx
@@ -2,18 +2,29 @@ import { useState } from "react";
 import TodoList from "./TodoList";
 
 export default function Todo() {
-  const [taskName, setTaskName] = useState("");
+  const [taskName, setTaskName] = useState({name:"", isCompleted:false});
   const [tasks, setTasks] = useState<any>([]);
 
   const addTask = () => {
     setTasks([...tasks, taskName]);
-    setTaskName("");
+    setTaskName({name:"", isCompleted:false});
   };
 
   const removeTask = (index: number) => {
     const newTasks = [...tasks];
     newTasks.splice(index,1);
     setTasks(newTasks);
+  }
+
+  const doneTask = (index: number, checked: boolean) => {
+    const newTasks = [...tasks];
+    // var taskCompleted=[...newTasks[index]];
+    var taskCompleted=newTasks[index];
+    taskCompleted.isCompleted=checked;
+    newTasks.splice(index,1);
+    newTasks.splice(index,0,taskCompleted);
+    setTasks(newTasks);
+    console.log(tasks);
   }
 
   return (
@@ -27,8 +38,8 @@ export default function Todo() {
           className="form-control"
           id="exampleFormControlInput1"
           placeholder="task name"
-          value={taskName}
-          onChange={(e) => setTaskName(e.target.value)}
+          value={taskName.name}
+          onChange={(e) => setTaskName({name: e.target.value, isCompleted:false})}
         />
       </div>
       <div className="d-grid">
@@ -36,7 +47,7 @@ export default function Todo() {
           Add Task
         </button>
       </div>
-      <TodoList remove={removeTask} tasks={tasks} />
+      <TodoList remove={removeTask} doneTask={doneTask} tasks={tasks} />
     </div>
   );
 }
